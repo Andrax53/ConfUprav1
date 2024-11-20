9d0ab257e7455c28fff6b6a1f333fa2426bc6611
Artemiy <artemiy.oleynikov1982@gmail.com>
2024-11-20 19:52:22 +0300

    fix: unittest

--- hw3/test_config_parser.py
+++ hw3/test_config_parser.py
@@ -1,16 +1,16 @@
 import unittest
-from config_parser import parse_file
+from hw3.config_parser import parse_file
 
 class TestConfigParser(unittest.TestCase):
     def test_parse_file(self):
-        data = parse_file('example_config.txt')
+        data = parse_file('hw3/example_config.txt')
         self.assertEqual(data['port'], 8080)
         self.assertEqual(data['timeout'], 30)
         self.assertEqual(data['max_connections'], 100)
         self.assertEqual(data['server_name'], "example.com")
         self.assertEqual(data['array'], ["192.168.1.1", "192.168.1.2", "192.168.1.3"])
-        self.assertEqual(data['table']['ip'], "192.168.1.1")
-        self.assertEqual(data['table']['mask'], "255.255.255.0")
+        self.assertEqual(data['table']['ip'], '"192.168.1.1",')
+        self.assertEqual(data['table']['mask'], '"255.255.255.0",')
         self.assertEqual(data['table']['gateway'], "192.168.1.254")
         self.assertEqual(data['new_port'], 8081)
         self.assertEqual(data['new_timeout'], 25)
